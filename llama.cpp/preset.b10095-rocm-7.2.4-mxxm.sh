efb7cffa4adaba62786d940cf8ae88b3577a8b6a
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2026-07-29 01:25:52 +0500

    llamacpp: add mxxm fork to daily builds (rocm 7.2.4)

---
+++ llama.cpp/preset.b10095-rocm-7.2.4-mxxm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export LLAMA_REPO='https://github.com/mxxm-t/mx-llama.cpp.git'
+export LLAMA_ROCM_VERSION='7.2.4'
+export LLAMA_BRANCH='b10095'
+export LLAMA_PRESET_NAME="${LLAMA_BRANCH}-rocm-${LLAMA_ROCM_VERSION}-mxxm"
